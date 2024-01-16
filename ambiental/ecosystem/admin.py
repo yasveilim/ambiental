3fe7da2384164b59a96dee837f80a4627c4bfad1
ismaelxyz <ismaelbeli.com@.com>
2024-01-16 00:33:12 -0400

    Fueron agregados nuevos modelos

--- ambiental/ecosystem/admin.py
+++ ambiental/ecosystem/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AmbientalBook, AmbientalBookProps
 
 # Register your models here.
+admin.site.register(AmbientalBook)
+admin.site.register(AmbientalBookProps)
--- ambiental/ecosystem/models.py
+++ ambiental/ecosystem/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class AmbientalBook(models.Model):
+    material = models.CharField(max_length=50)
+    document_name = models.CharField(max_length=50)
+    note = models.TextField()
+
+class AmbientalBookProps(models.Model):
+    book = models.ForeignKey(AmbientalBook, on_delete=models.CASCADE)
+    ADVANCE_STATUS = [
+        ("delivered", "DELIVERED"),
+        ("pending", "PENDING"),
+        ("na", "NA")
+    ]
+    archive = models.FileField(upload_to='uploads/') 
+    comment = models.TextField()
+    advance = models.CharField(
+        max_length=9, choices=ADVANCE_STATUS, default="pending")
+    essential_in_cloud = models.BooleanField(default=True)
