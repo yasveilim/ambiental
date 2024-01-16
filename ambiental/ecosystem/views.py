29fcf583e92fff40cdc428f805c807d3883edb58
yasveilim <monicayasveili@gmail.com>
2024-01-15 22:51:34 -0500

    Cambios al agregar a views la ruta de la imagen

--- ambiental/ecosystem/views.py
+++ ambiental/ecosystem/views.py
@@ -16,7 +16,7 @@ class Index(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['category'] = self.kwargs.get('site')
-        
+        context['imgmaterial'] = 'agua.jpg'
         return context
     
 
