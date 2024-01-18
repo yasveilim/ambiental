afaab96bf05e1954c90396e8ee8199f143c2852c
ismaelxyz <ismaelbeli.com@.com>
2024-01-17 20:28:28 -0400

    Tareas para monica

--- sharepoint.py
+++ sharepoint.py
@@ -102,8 +102,31 @@ def autenticate() -> Account:
 
 def read_sicma_db(account: Account):
     workbook = load_workbook(account, 'root:sites/Ambiental:/Requerimientos de informacion V22 NDA1.xlsx')
-    print(workbook)
-    print(read_all_cells(workbook, 'CRITICAS'))
+    #print(workbook)
+
+    # 1) Convertir este archivo en un modulo.
+    # 2) Ignorar todas las celdas hasta (guiate del indice):
+    #   ['MATERIA', 'ID+', 'DOCUMENTO', 'INDISPENSABLE SUBIR A LA NUBE PREVIO A LA AUDITORÍA', 'AVANCE', '', '', 'ARCHIVOS', 'COMENTARIOS', '']
+    # Verifica si para las hojas de interes el indice es el mismo que en la hoja de 'CRITICAS'
+
+    # ['AIRE', 1, 'Licencia Ambiental Única o Licencia de funcionamiento (Actualizada)', 1, '', 1, '', '', '', '']
+    # ['', 3, 'Cédula de operación (Últimos 2 años). Incluir constancia de recepción, respaldo y diagramas. Incluye reporte RETC', 1, '', 1, '', '', '', ''] 
+    # 3) Procesar los datos en forma de diccionario para generar la siguiente estructura:
+    x = [
+        {
+            'material': 'AIRE',
+            'documents': [
+                ['Licencia Ambiental Única o Licencia de funcionamiento (Actualizada)', True, 'pending', '', '']
+                ['Cédula de operación (Últimos 2 años). Incluir constancia de recepción, respaldo y diagramas. Incluye reporte RETC', True, 'pending', '', ''] 
+            ] 
+        },
+        {
+            'material': 'RUIDO',
+            'documents': []
+        }
+    ]
+    for row in read_all_cells(workbook, 'CRITICAS'):
+        print(row)
 
 
 def main():
