0e37f60701dd15bd7991c4ee2ffbf9bf22f76000
ismaelxyz <ismaelbeli.com@.com>
2024-01-24 13:22:59 -0400

    base for multiple documents

--- ambiental/static/js/des.js
+++ ambiental/static/js/des.js
@@ -1,4 +1,4 @@
-const OPTION_MENU = document.querySelector(".select-menu"),
+const OPTION_MENU = document.querySelectorAll(".select-menu"),
        SELECT_BTN = OPTION_MENU.querySelector(".select-btn"),
        OPTIONS    = OPTION_MENU.querySelectorAll(".option"),
        SBTN_TEXT  = OPTION_MENU.querySelector(".sBtn-text");
