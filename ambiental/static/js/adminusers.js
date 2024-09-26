5d5007528671c09f3fe15b8cfa17deb7e9935372
ismaelxyz <ismaelbeli.com@gmail.com>
2024-09-25 21:37:48 -0400

    feat added user base and: fix delete user

--- ambiental/static/js/adminusers.js
+++ ambiental/static/js/adminusers.js
@@ -1,3 +1,58 @@
+async function newUser() {
+  let csrftoken = Cookies.get("csrftoken");
+  let config = {
+    headers: {
+      "X-CSRFToken": csrftoken,
+    },
+  };
+
+  const topBar = document.querySelector(".top-bar ");
+  let rightSide = document.querySelector(".right-side");
+  let rightSideNewUser = document.querySelector(".right-side-new-user");
+
+  if (!rightSideNewUser) {
+    rightSideNewUser = createRightSideNewUser(topBar);
+  }
+
+  if (rightSide) {
+    rightSide.remove();
+  }
+
+  let newUserObject = {
+    username: "",
+    lastname: "",
+    email: ""
+  };
+
+  const createUser = () => {};
+  const cancelCreateUser = () => {};
+
+  const userFieldsDiv = document.querySelector(".container-form__group");
+
+  setUserFieldsDiv(
+    userFieldsDiv, newUserObject, createUser, cancelCreateUser
+  );
+}
+
+function createRightSideNewUser(topBar) {
+  let rightSide = document.createElement("div");
+  rightSide.classList.add("right-side");
+
+  let updateButton = document.createElement("button");
+  updateButton.textContent = "Crear";
+
+  updateButton.classList.add("add-user-button");
+  rightSide.appendChild(updateButton);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.textContent = "Cancelar";
+  deleteButton.classList.add("cancel-add-user-button");
+  rightSide.appendChild(deleteButton);
+
+  topBar.appendChild(rightSide);
+  return rightSide;
+}
+
 async function selectUser(event, userID) {
   let csrftoken = Cookies.get("csrftoken");
   let config = {
@@ -15,7 +70,6 @@ async function selectUser(event, userID) {
   user = responseData.data;
 
   const topBar = document.querySelector(".top-bar ");
-
   let rightSide = document.querySelector(".right-side");
 
   const updateUser = () => {
@@ -39,33 +93,44 @@ async function selectUser(event, userID) {
   };
 
   const deleteUser = () => {
-    console.log("Deleting user: ", userID, config);
+    axios.delete(`/api/admin-users/${userID.id}/`, config).then((response) => {
+      console.log("response=>", response);
 
-    axios.delete(`/api/admin-users/${userID.id}/`, config);
+      const btnId = `#user-card-${userID.id}`;
+      const btn = document.querySelector(btnId);
+      btn.remove();
+    });
   };
 
   if (!rightSide) {
-    rightSide = createRightSide(topBar, updateUser, deleteUser);
+    rightSide = createRightSide(topBar);
   }
+
+  const updateButton = rightSide.querySelector(".update-button");
+  updateButton.onclick = updateUser;
+
+  const deleteButton = rightSide.querySelector(".delete-button");
+  deleteButton.onclick = deleteUser;
 
   const userFieldsDiv = document.querySelector(".container-form__group");
 
-  setUserFieldsDiv(userFieldsDiv, user);
+  setUserFieldsDiv(userFieldsDiv, user, updateUser, deleteUser);
 }
 
-function createRightSide(topBar, onUpdate, onDelete) {
+function createRightSide(topBar) {
   let rightSide = document.createElement("div");
   rightSide.classList.add("right-side");
 
   let updateButton = document.createElement("button");
   updateButton.textContent = "Actualizar";
-  updateButton.onclick = onUpdate;
+  //updateButton.onclick = onUpdate;
+
   updateButton.classList.add("update-button");
   rightSide.appendChild(updateButton);
 
   let deleteButton = document.createElement("button");
   deleteButton.textContent = "Borrar";
-  deleteButton.onclick = onDelete;
+  //deleteButton.onclick = onDelete;
   deleteButton.classList.add("delete-button");
   rightSide.appendChild(deleteButton);
 
