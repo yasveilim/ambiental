3a465639cb7f328dea9b22ece0d656d3d81e8c4a
ismaelxyz <ismaelbeli.com@gmail.com>
2024-12-02 11:11:30 +0000

    feat: Integration of the statistics chart in the advance script

--- ambiental/static/js/advance.js
+++ ambiental/static/js/advance.js
@@ -1,5 +1,53 @@
+var myChart = null;
+
+function createChart(labels, values) {
+    const ctx = document.getElementById("nvivoBarChart").getContext("2d");
+    if (myChart) {
+        myChart.clear();
+        myChart.destroy();
+    }
+
+    myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: labels,
+            datasets: [
+                {
+                    label: "Avances en la entrega de documentos",
+                    data: values,
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                },
+            },
+        },
+    });
+}
+
+function SetChart(categoryAndTotal) {
+    const canvasStats = document.querySelector("#nvivoBarChart");
+    const context = canvasStats.getContext("2d");
+    context.clearRect(0, 0, canvasStats.width, canvasStats.height);
+
+    let labels = [];
+    let values = [];
+
+    categoryAndTotal.forEach((item) => {
+        labels.push(item.category);
+        values.push(item.total);
+    });
+
+
+    createChart(labels, values);
+
+}
+
 async function setAdvanceTable(categories) {
-    console.log();
     let loaderDiv = document.querySelector("div.loader-div");
     loaderDiv.style.display = "block";
     const tableBody = document.querySelector(".tbody-advance");
@@ -8,6 +56,7 @@ async function setAdvanceTable(categories) {
         `/api/statistics/materials/${targetUser.id}`
     );
     const materialData = Object.values(request.data);
+    const categoryAndTotal = [];
 
     Object.entries(categories).forEach(
         ([category, categorName], index) => {
@@ -32,18 +81,29 @@ async function setAdvanceTable(categories) {
                       <td>${currentMaterialData["total"]}</td>
                   `;
             tableBody.appendChild(tr);
+
+            let totalRequired = parseFloat(currentMaterialData["total_required"]);
+            let na = parseFloat(currentMaterialData["na"]);
+            let totalPercentage = parseFloat(currentMaterialData["total"].replace('%', ''));
+
+
+            let applicableRequirements = totalRequired - na;
+            let absoluteTotal = (totalPercentage / 100) * applicableRequirements;
+
+            categoryAndTotal.push({
+                category: categorName,
+                total: absoluteTotal,
+            });
         }
     );
+
+    SetChart(categoryAndTotal);
 
     loaderDiv.style.display = "none";
 }
 
 axios.get("/api/category/").then(async (response) => {
     const categories = response.data;
-    console.log(categories);
-
-
-    /**/
 
     let selectUser = document.querySelector("#users-list");
     if (selectUser !== null && selectUser !== undefined) {
@@ -55,3 +115,7 @@ axios.get("/api/category/").then(async (response) => {
         });
     }
 });
+
+
+
+
